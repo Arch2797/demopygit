bfa4d9980fa92831c692c26b474731bb0acf80a6
Archana <archana.gawari@fafadiatech.com>
2021-11-17 16:20:23 +0530

    added pattern printing function

--- demo.py
+++ demo.py
@@ -1,2 +1,10 @@
-print("hello")
-print("cloning from github")
+def pypart(n):
+    myList = []
+    for i in range(1, n+1):
+        myList.append("*"*i)
+    print("\n".join(myList))
+
+
+# Driver Code
+n = 5
+pypart(n)
